53515bd75ae96bb6894bf6fd1b2553ae4a6df4ea
thomasyu888 <thomas.yu@sagebase.org>
2021-03-26 00:16:02 +0800

    Add question param

--- score.py
+++ score.py
@@ -16,13 +16,31 @@ def get_args():
                         help="Submission file")
     parser.add_argument("-r", "--results", required=True,
                         help="Scores output file")
-
+    parser.add_argument("-q", "--question", required=True,
+                        type=int, help="Question number")
     return parser.parse_args()
+
+
+def scoring_function_a(pred, gold):
+    """Example scoring funtion 1"""
+    score1 = 0.8
+    score2 = 0.2
+    return score1, score2
+
+
+def scoring_function_b(pred, gold):
+    """Example scoring function 2"""
+    score1 = 0.5
+    score2 = 0.3
+    return score1, score2
 
 
 def main():
     """Main function."""
     args = get_args()
+
+    scoring_func_mapping = {1: scoring_function_a,
+                            2: scoring_function_b}
 
     with open(args.submission_file, "r") as sub_file, \
             open(args.goldstandard, "r") as gold_file:
@@ -30,8 +48,9 @@ def main():
         gold = gold_file.read()
 
         ## Sample scoring ##
-        score1 = 0.8
-        score2 = 0.2
+        score1, score2 = scoring_func_mapping[args.question](
+            pred, gold
+        )
         prediction_file_status = "SCORED"
 
     # secondary_metric and secondary_metric_value are optional
